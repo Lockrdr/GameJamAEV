ba664270200f6fd73bf6a0c4f1bf16d092ff0b9f
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-06-03 21:00:42 +0200

    player combar con add life y substract life

--- GameJamAEV/Assets/Scripts/PlayerCombat.cs
+++ GameJamAEV/Assets/Scripts/PlayerCombat.cs
@@ -5,10 +5,39 @@ public class PlayerCombat : MonoBehaviour {
 
     public float m_playerHealth = 0f;
 
-    public enum PlayerState { Alive = 0, Dead = 1};
+    public enum PlayerState { Alive = 0, Dead = 1, Resurrected = 2};
     public PlayerState m_playerState = 0;
 
 
+    public void substractLife(float amount)
+    {
+        m_playerHealth -= amount;
+        if(m_playerHealth <= 0)
+        {
+            m_playerState = PlayerState.Dead;
+            Debug.Log("Jugador con menos de 0 de vida. Estado Dead");
+        }
+    }
+
+
+    public void addLife(float amount)
+    {
+        m_playerHealth += amount;
+
+        if (m_playerHealth >= 100)
+        {
+            m_playerState = PlayerState.Resurrected;
+            Debug.Log("Jugador revivido");
+
+        }else if(m_playerHealth > 0)
+        {
+            m_playerState = PlayerState.Alive;
+            Debug.Log("Jugador con mas de 0 de vida. Estado Alive");
+        }
+        
+        
+    }
+    
 	void Start () {
 	
 	}
@@ -16,5 +45,6 @@ public class PlayerCombat : MonoBehaviour {
 	
 	void Update () {
 	
+
 	}
 }
