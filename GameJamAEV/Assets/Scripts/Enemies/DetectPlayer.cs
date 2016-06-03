41793c2db9b9cf29faa9ec6c53325ad43beba079
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-06-03 23:01:13 +0200

    Detección de player por parte del enemigo hecha--- reorganizadas carpetas

---
+++ GameJamAEV/Assets/Scripts/Enemies/DetectPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectPlayer : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.GetComponentInParent<Enemy>().playerDetected();
+
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.GetComponentInParent<Enemy>().playerLost();
+
+        }
+    }
+
+
+   
+}
---
+++ GameJamAEV/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+    public bool m_playerDetected = false;
+
+    public float m_enemyHP = 50f;
+    
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+	
+        if(m_playerDetected)
+        {
+            attackPlayer();
+        }
+	}
+
+    void attackPlayer()
+    {
+        //if(m_cooldownBetwe)
+        //{
+
+        //}
+    }
+
+    public void playerDetected()
+    {
+        m_playerDetected = true;
+        Debug.Log("Te veo");
+    }
+
+    public void playerLost()
+    {
+        m_playerDetected = false;
+        Debug.Log("No te veo");
+
+    }
+
+
+}
