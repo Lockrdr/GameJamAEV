8f5fc91d90dd99252cfff1859256d07c12092ce1
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-06-04 00:07:47 +0200

    esqueleto enemigos patrolling

---
+++ GameJamAEV/Assets/Scripts/Enemies/PatrollingEnemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrollingEnemy : Enemy {
+
+    [Header("Patrolling variables")]
+    public Transform waypoint1;
+    public Transform waypoint2;
+    private Vector3 mDir;
+    public Transform currentWaypoint;
+
+	void Start () {
+        currentWaypoint = waypoint2;	
+	}
+
+
+    void Update()
+    {
+        if (m_playerDetected)
+        {
+            attackPlayer();
+        }
+        m_timeSinceLastAttack -= Time.deltaTime;
+        
+        //mDir = newPostionTransform - transform.position;
+        //mDir.Normalize();
+        //mDir.x = 0;
+        //transform.position += m_elevatorSpeed * Time.deltaTime * mDir;
+
+        ////Debug.Log(Mathf.Abs(Vector3.Distance(transform.position, newPostionTransform)));
+        //if (elevatorMoving && Mathf.Abs(Vector3.Distance(transform.position, newPostionTransform)) < Mathf.Abs(0.01f))
+        //{
+
+        //    Debug.Log("llege a destino");
+
+        //    elevatorMoving = false;
+        //    StartCoroutine(nextTarget());
+        //}
+    }
+}
---
+++ GameJamAEV/Assets/Scripts/Enemies/StaticEnemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticEnemy : Enemy {
+
+    void Update()
+    {
+        if (m_playerDetected)
+        {
+            attackPlayer();
+        }
+
+        m_timeSinceLastAttack -= Time.deltaTime;
+    }
+}
