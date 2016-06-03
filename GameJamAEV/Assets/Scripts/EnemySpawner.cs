e798f3e461b7098bc0198c13fe9202787d266b47
Iulen <im_993@hotmail.com>
2016-06-04 01:15:55 +0200

    Spawn de enemigos básico

---
+++ GameJamAEV/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject[] spawnPoints;
+	public GameObject[] enemies;
+	int[] positions;
+	int spawnPosition, spawnEnemy;
+
+	// Use this for initialization
+	void Start () {
+		spawnWave (0);
+	}
+	
+	// Update is called once per frame
+	public void spawnWave(int wave){
+
+		positions = new int[spawnPoints.Length];
+		spawnPosition = Random.Range (0,spawnPoints.Length-1);
+		spawnEnemy = Random.Range (0, enemies.Length - 1);
+		Instantiate (enemies [spawnEnemy], spawnPoints [spawnPosition].transform.position, Quaternion.identity);
+	}
+
+
+}
