9e505f6727a17b6db22913621f133c09ab84ecaf
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-06-03 20:25:45 +0200

    gameManager statico y singleton

--- GameJamAEV/Assets/Scripts/GameManager.cs
+++ GameJamAEV/Assets/Scripts/GameManager.cs
@@ -3,9 +3,19 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    private static GameManager m_instance;
+
+    public static GameManager getInstance()
+    {
+        return m_instance;
+    }
+
+
+    public int m_WaveNumber = 1;
 
 	void Start () {
-	
+        if (m_instance == null)
+            m_instance = this;
 	}
 	
 	
