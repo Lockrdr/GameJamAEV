ddb5302d0959fcd8c0b26fd82e309a1ee4b96eb8
Iulen <im_993@hotmail.com>
2016-06-03 21:12:52 +0200

    Orbe

---
+++ GameJamAEV/Assets/Scripts/Orb/OrbMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbMovement : MonoBehaviour {
+
+	public float rotationSpeed = 1.75f;
+	public float rotationRadius = 1.5f;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		if(player == null){
+			Debug.Log ("Jugador no encontrado");
+		}
+		transform.position = player.transform.position + Vector3.right*rotationRadius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.RotateAround(player.transform.position, transform.forward, rotationSpeed);
+		transform.rotation = Quaternion.identity;
+
+	}
+}
