aad22609f4081d86f57d6d088f80b5d791e11c01
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-06-03 20:49:39 +0200

    script de player combat

---
+++ GameJamAEV/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCombat : MonoBehaviour {
+
+    public float m_playerHealth = 0f;
+
+    public enum PlayerState { Alive = 0, Dead = 1};
+    public PlayerState m_playerState = 0;
+
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+	
+	}
+}
