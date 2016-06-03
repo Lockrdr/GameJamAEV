1bb0802ab78fc408f1f3b187b4c7a04241e87728
Iulen <im_993@hotmail.com>
2016-06-03 22:39:17 +0200

    Escena de combate
    Disparo acabado

--- GameJamAEV/Assets/Scripts/Orb&Shoot/ShootMoment.cs
+++ GameJamAEV/Assets/Scripts/Orb&Shoot/ShootMoment.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class ShootMoment : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	private Vector3 shootDirection;
+	public float shootSpeed = 1f;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D> ();
+
+		shootDirection = Input.mousePosition;
+		shootDirection.z = 0.0f;
+		shootDirection = Camera.main.ScreenToWorldPoint (shootDirection);
+		shootDirection = shootDirection - transform.position;
+		rb.AddForce (shootDirection.normalized*1000*shootSpeed);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
