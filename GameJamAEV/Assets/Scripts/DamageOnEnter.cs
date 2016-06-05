5c9c80aebcd030b8c40561ecdb09caefb70b7feb
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-06-05 02:04:23 +0200

    following enemy y enemigos te hcaen daños al tocarlos

---
+++ GameJamAEV/Assets/Scripts/DamageOnEnter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnEnter : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            coll.gameObject.GetComponent<PlayerCombat>().addLife(25f);
+        }
+    }
+}
---
+++ GameJamAEV/Assets/Scripts/Enemies/FollowingEnemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingEnemy : Enemy {
+
+
+    public float movingSpeed = 2f;
+
+    private Vector3 mDir;
+    public Vector3 newPostionTransform;
+    private GameObject playerGO;
+
+	void Start () {
+        base.Start();
+        playerGO = GameObject.FindGameObjectWithTag("Player").gameObject;
+	}
+ 
+	
+	void Update () {
+
+        newPostionTransform = playerGO.transform.position;
+        mDir = newPostionTransform - transform.localPosition;
+        mDir.Normalize();
+        mDir.z = 0;
+        transform.localPosition += movingSpeed * Time.deltaTime * mDir;
+
+
+        //Debug.Log(Mathf.Abs(Vector3.Distance(transform.position, newPostionTransform)));
+        //if (Mathf.Abs(Vector3.Distance(transform.localPosition, newPostionTransform)) < Mathf.Abs(0.1f))
+        //{
+
+        //    //Debug.Log("llege a destino");
+        //    changeDirection();
+        //    //StartCoroutine(nextTarget());
+        //}
+	}
+}
